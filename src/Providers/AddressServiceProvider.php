beb48a4e66055c9dfb9ba72cd89f1addc6db8ddf
railken <railken@protonmail.ch>
2019-03-13 16:39:23 +0100

    add geolocation

--- src/Providers/AddressServiceProvider.php
+++ src/Providers/AddressServiceProvider.php
@@ -2,8 +2,20 @@
 
 namespace Railken\Amethyst\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Railken\Amethyst\Common\CommonServiceProvider;
+use Railken\Amethyst\Managers\AddressManager;
+use Railken\Amethyst\Models\Address;
 
 class AddressServiceProvider extends CommonServiceProvider
 {
+    /**
+     * @inherit
+     */
+    public function register()
+    {
+        parent::register();
+
+        Config::set('amethyst.geolocation.data.geolocation-point.attributes.localizable.options.'.Address::class, AddressManager::class);
+    }
 }
