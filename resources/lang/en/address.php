2bfb70f2368da06c382e95330708e57e1eaf1e27
railken <railken@protonmail.ch>
2019-04-06 22:40:22 +0200

    add lang

---
+++ resources/lang/en/address.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'name' => 'Address',
+    'description' => 'The place where a person or organization can be found or communicated with.  Contains fields such as: street, postal code, city, country etc... Can be used for example as a shipment address or as an invoice address.',
+    'attributes' => [
+        'street' => [
+            'label' => 'Street'
+        ],
+        'zip_code' => [
+            'label' => 'Postal Code'
+        ],
+        'city' => [
+            'label' => 'City'
+        ],
+        'province' => [
+            'label' => 'Province'
+        ],
+        'country' => [
+            'label' => 'Country'
+        ]
+    ]
+]
---
+++ resources/lang/it/address.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'name' => 'Indirizzo',
+    'description' => 'Indirizzo',
+    'attributes' => [
+        'street' => [
+            'label' => 'Via'
+        ],
+        'zip_code' => [
+            'label' => 'Codice postale'
+        ],
+        'city' => [
+            'label' => 'Città'
+        ],
+        'province' => [
+            'label' => 'Provincia'
+        ],
+        'country' => [
+            'label' => 'Nazione'
+        ]
+    ]
+]
