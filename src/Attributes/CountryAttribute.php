602cb70f8bd9dfc9be254be8523d45ae626873a5
railken <railken@protonmail.ch>
2019-06-23 17:26:33 +0200

    Country as Enum

--- src/Attributes/CountryAttribute.php
+++ src/Attributes/CountryAttribute.php
@@ -2,10 +2,11 @@
 
 namespace Railken\Amethyst\Attributes;
 
-use Railken\Lem\Attributes\TextAttribute;
+use Railken\Lem\Attributes\EnumAttribute;
 use Railken\Lem\Contracts\EntityContract;
+use League\ISO3166\ISO3166;
 
-class CountryAttribute extends TextAttribute
+class CountryAttribute extends EnumAttribute
 {
     /**
      * Name attribute.
@@ -13,6 +14,21 @@ class CountryAttribute extends TextAttribute
      * @var string
      */
     protected $name = 'country';
+
+    /**
+     * Create a new instance.
+     *
+     * @param string $name
+     * @param array  $options
+     */
+    public function __construct(string $name = null, array $options = [])
+    {
+        $options = array_map(function ($item) {
+            return $item['alpha2'];
+        }, (new ISO3166)->all());
+
+        parent::__construct($name, $options);
+    }
 
     /**
      * Is a value valid ?
@@ -25,7 +41,7 @@ class CountryAttribute extends TextAttribute
     public function valid(EntityContract $entity, $value)
     {
         try {
-            (new \League\ISO3166\ISO3166())->alpha2($value);
+            (new ISO3166)->alpha2($value);
 
             return true;
         } catch (\Exception $e) {
@@ -40,6 +56,6 @@ class CountryAttribute extends TextAttribute
      */
     public function getType()
     {
-        return 'Text';
+        return 'Enum';
     }
 }
