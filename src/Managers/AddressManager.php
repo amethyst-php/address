7b7be6e82f1f7993a2f3ffd83fe7b9ab53a093b1
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/AddressManager.php
+++ src/Managers/AddressManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Address newEntity()
+ * @method \Amethyst\Schemas\AddressSchema getSchema()
+ * @method \Amethyst\Repositories\AddressRepository getRepository()
+ * @method \Amethyst\Serializers\AddressSerializer getSerializer()
+ * @method \Amethyst\Validators\AddressValidator getValidator()
+ * @method \Amethyst\Authorizers\AddressAuthorizer getAuthorizer()
+ */
 class AddressManager extends Manager
 {
     use ConfigurableManager;
