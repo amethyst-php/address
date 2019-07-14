d83b271ee008df6aeef7e9ed5267f1dc7840a981
railken <railken@protonmail.ch>
2019-07-14 13:06:31 +0200

    fix style

--- src/Managers/AddressManager.php
+++ src/Managers/AddressManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Address newEntity()
- * @method \Amethyst\Schemas\AddressSchema getSchema()
+ * @method \Amethyst\Models\Address                 newEntity()
+ * @method \Amethyst\Schemas\AddressSchema          getSchema()
  * @method \Amethyst\Repositories\AddressRepository getRepository()
- * @method \Amethyst\Serializers\AddressSerializer getSerializer()
- * @method \Amethyst\Validators\AddressValidator getValidator()
- * @method \Amethyst\Authorizers\AddressAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\AddressSerializer  getSerializer()
+ * @method \Amethyst\Validators\AddressValidator    getValidator()
+ * @method \Amethyst\Authorizers\AddressAuthorizer  getAuthorizer()
  */
 class AddressManager extends Manager
 {
